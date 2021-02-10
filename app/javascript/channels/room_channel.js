2943c9274de1284ad741ebc792ffbd8aaad0f7c0
Tomoaki Hirata <t.hirata.0463@gmail.com>
2021-02-10 17:37:35 +0900

    action cable create

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -3,6 +3,14 @@ import consumer from "./consumer"
 consumer.subscriptions.create("RoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    document.querySelector('input[data-behavior="room_speaker"]').addEventListener('keypress', (event) => {
+      console.log(event.key);
+      if (event.key === 'Enter') {
+        this.speak(event.target.value);
+        event.target.value = '';
+        return event.preventDefault();
+      }
+    });
   },
 
   disconnected() {
@@ -10,10 +18,15 @@ consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
+    alert('received');
     // Called when there's incoming data on the websocket for this channel
+    console.log('received');
+    const element = document.querySelector('#messages');
+    console.log(element);
+    element.insertAdjacentHTML('beforeend', data['message']);
   },
-
-  speak: function() {
-    return this.perform('speak');
+  
+  speak: function(message) {
+    return this.perform('speak', {message: message});
   }
 });
